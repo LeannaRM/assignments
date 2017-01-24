0ba23a416c177530eb9d3db972edc9df99099772
Leanna McDonald <leanna.rose.mcdonald@gmail.com>
2017-01-24 16:05:48 -0600

    Gets outflow and inflow into one variable 'money', ready for hash

--- csv-report/csv-report.rb
+++ csv-report/csv-report.rb
@@ -7,18 +7,42 @@ require 'csv'
 #CSV.foreach("accounts.csv", {headers: true, return_headers: false}) do |row|
 #    puts row["Inflow"].inspect
 #end
-
+categoryHashPriya = Hash.new
 
 CSV.foreach("accounts.csv", {headers: true, return_headers: false}) do |row|
     if row["Account"] == "Priya"
-		puts row.inspect
+		#puts row.inspect
+		
+		#money = row[Inflow] - row[Outflow]
+		#money = "$5000.00" - "$0.00"
+		outflow = row["Outflow"].delete ","
+		inflow = row["Inflow"].delete ","
+		outflow = outflow.delete "$"
+		inflow = inflow.delete "$"
+
+		puts "\n"
+		puts outflow
+		puts inflow
+
+		print money = inflow.to_f - outflow.to_f
+		#print \n
+
+
+		#categoryHashPriya[row["Category"]] = money
+
+		#puts row["Outflow"].to_I.sum
 	end
 end
 
 
 
 CSV.foreach("accounts.csv", {headers: true, return_headers: false}) do |row|
-    if row["Account"] == "Sonia" || row["Account"] == "Sonia\n"
-		puts row.inspect
+    if row["Account"] == "Sonia\n"
+    	row["Account"] = "Sonia"
+    	#puts row.inspect
+    elsif row["Account"] == "Sonia"
+		#puts row.inspect
 	end
+	#puts row["Outflow"].delete "$"
+	#puts row["Inflow"].delete "$"
 end
