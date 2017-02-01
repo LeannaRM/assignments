3b3f52f2d688423d3921402d7f72dda1c7235224
Leanna McDonald <leanna.rose.mcdonald@gmail.com>
2017-02-01 10:22:17 -0600

    can do a series

--- CLI_RockPaperScissors/RPS.js
+++ CLI_RockPaperScissors/RPS.js
@@ -4,25 +4,24 @@ var score = [0,0];
 
 	var prompt = require('prompt');
 	prompt.start();
-var i = 0
-while (i<3){
-	score = playRound(score)
-	i += 1
-}
+playRound()
 
 
-function playRound(score){
+function playRound(){
 	prompt.get(['P1'], function (err, result) {
 		p1_weapon = result.P1.toLowerCase()
 
 		if (p1_weapon == "rock" || p1_weapon == "paper" || p1_weapon == "scissors"){
 			prompt.get(['P2'], function (err, result) {
 				p2_weapon = result.P2.toLowerCase()
-				if (p2_weapon == "paper"|| p2_weapon == "paper" || p2_weapon == "scissors"){
+				if (p2_weapon == "paper"|| p2_weapon == "rock" || p2_weapon == "scissors"){
 					result = functions.determineWinnerFunction(p1_weapon,p2_weapon)
 					score = functions.updateScoreFunction(score, result)
 					console.log(result)
 					console.log("Current score:\n Player 1: " + score[0] + " Player 2: " + score[1])
+					if (score[0] < 3 && score[1]< 3){
+						playRound()
+					}
 				}
 				else{console.log("Player 2 please enter rock, paper, or scissors")}
 			});
