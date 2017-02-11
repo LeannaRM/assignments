224f590c8fe9dd39527283bc985ec7c0a86fa7c8
Leanna McDonald <leanna.rose.mcdonald@gmail.com>
2017-02-10 22:07:08 -0600

    final

--- quizgame/scripts.js
+++ quizgame/scripts.js
@@ -110,22 +110,3 @@ window.addEventListener("load", function (){
 	];
 
 });
-
-
-
-
-
-
-		// var txtFile = "./questions.txt"
-		// var file = new File(txtFile);
-
-		// file.open("r"); // open file with read access
-		// var str = "";
-		// while (!file.eof) {
-		// 	// read each line of text
-		// 	str += file.readln() + "\n";
-		// }
-		// file.close();
-		// alert(str);
-
-
