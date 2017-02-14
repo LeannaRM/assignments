19a38ff848573963313fe2aa859152def1232524
Leanna McDonald <leanna.rose.mcdonald@gmail.com>
2017-02-14 10:49:03 -0600

    able to click on saved paintings and reload

--- mondrian/public/scripts.js
+++ mondrian/public/scripts.js
@@ -3,13 +3,15 @@ window.addEventListener("load", function (){
 	colorhash = {
 		"red": "rgb(204, 0, 0)",
 		"blue": "rgb(0, 0, 204)",
-		"yellow": "rgb(255, 236, 0)"
+		"yellow": "rgb(255, 236, 0)",
+		"white": "rgb(255, 255, 255)"
 	}
 	colorhashinverted = {
 		"rgb(204, 0, 0)": "red",
 		"rgb(0, 0, 204)": "blue",
 		"rgb(255, 236, 0)": "yellow"
 	}
+
 	var chooseColorTrigger = document.getElementsByClassName("color");
 	for (i=0;i<chooseColorTrigger.length;i++){
 		chooseColorTrigger[i].addEventListener("click",choosecolor);
@@ -20,6 +22,7 @@ window.addEventListener("load", function (){
 		currentcolor = computedStyle.getPropertyValue('background-color')
 	}
 
+
 	var chooseBoxTrigger = document.getElementsByClassName("row");
 	for (i=0;i<chooseBoxTrigger.length;i++){
 		chooseBoxTrigger[i].addEventListener("click",setcolor);
@@ -28,6 +31,7 @@ window.addEventListener("load", function (){
 	function setcolor(e){
 		e.target.style.backgroundColor = currentcolor;
 	}
+
 
 	var saveTrigger = document.getElementById("save_button");
 	saveTrigger.addEventListener("click",savePainting);
@@ -50,17 +54,14 @@ window.addEventListener("load", function (){
 			boxcolors[box] = color;
 			querystring = querystring + "&" + box + "=" + color;
 		}
+
 		querystring = querystring.replace(/\s/g, "");
 		ourRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-
 		ourRequest.send(querystring);
-
-
 
 		htmlstring = "<a href='#' class='saveddata'>" + n + "</a>"
 		container = document.getElementsByClassName("saveddata_container")[0];
 		container.insertAdjacentHTML('beforeend',htmlstring)
-
 
 		e.preventDefault();
 	}
@@ -72,7 +73,7 @@ window.addEventListener("load", function (){
 			datename = data[i]["date"]
 			date = datename.substr(5,5)
 			time = datename.substr(11,5)
-			htmlstring = "<a href='#' class='saveddata'>" + date + " at " + time + "</a>"
+			htmlstring = "<a href='#' class='saveddata' id = " + i + ">" + date + " at " + time + "</a>"
 			container = document.getElementsByClassName("saveddata_container")[0];
 			container.insertAdjacentHTML('beforeend',htmlstring)
 		i += 1
@@ -85,13 +86,38 @@ window.addEventListener("load", function (){
 		ourRequest.onload = function() {
 			var ourdata = JSON.parse(ourRequest.responseText)
 			createlist(ourdata);
+			makeTriggers();
 		}
 		ourRequest.send()
 	}
-
-
+	
 	createSavedDataList();
 
+	function makeTriggers(){
+		var savedPaintingTrigger = document.getElementsByClassName("saveddata");
+		for (i=0;i<savedPaintingTrigger.length;i++){
+			savedPaintingTrigger[i].addEventListener("click",showSavedPainting);
+		}
+	}
+
+	function showSavedPainting(e){
+		numberPainting = e.target.getAttribute('id');
+		var ourRequest = new XMLHttpRequest();
+		ourRequest.open('GET', '/saveddata')
+		ourRequest.onload = function() {
+			var ourdata = JSON.parse(ourRequest.responseText)
+			showPainting(ourdata[numberPainting]);
+		}
+		ourRequest.send()
+	}
+	function showPainting(data) {
+		boxes = document.getElementsByClassName("row")
+		for (i=0;i<boxes.length;i++) {
+			id = boxes[i].getAttribute('id')
+			rgbcolor = colorhash[data[id]]
+			boxes[i].style.backgroundColor = rgbcolor
+		}
+	}
 
 
 });
